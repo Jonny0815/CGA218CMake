07dca7476952100416c944c43f51c3788414ecc8
Johannes Hitzinger <jonny0815ger@gmail.com>
2021-01-14 17:39:21 +0100

    Aufgabe6 update, fragment shader faulty

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -1,11 +1,27 @@
-#version 330 core
+#version 330
+in vec3 Normal, LightDir, ViewDir;
+uniform vec3 matDiffuse, lightColor;
+uniform vec3 lightColorAmbient;
+uniform vec3 matEmissive;
+uniform mat3 matSpecular;
+uniform float matShininess;
+out vec4 color;  
 
-in vec3 colorVS;
-
-//fragment shader output
-out vec4 color;
-
-void main(){
-    color = vec4(colorVS.x, colorVS.y, colorVS.z, 1.0);
+void main() {
+// normalize everything necessary // 
+vec3 N = normalize(Normal);
+vec3 L = normalize(LightDir);
+// diffuse component //
+float cosa = max(0.0, dot(N,L));
+vec3 DiffuseTerm = matDiffuse * lightColor;   
+color = vec4( DiffuseTerm * cosa, 1.0);
+vec3 AmbientEmissiveTerm = matEmissive + matDiffuse * lightColorAmbient; 
+color += vec4(AmbientEmissiveTerm, 0.0); 
+vec3 V = normalize(ViewDir);
+vec3 R = normalize(reflect(-L,N));
+float cosBeta = max(0.0, dot(R,V));
+float cosBetak = pow(cosBeta, matShininess);
+vec3 SpecularTerm = matSpecular * lightColor;
+vec3 FinalTerm = SpecularTerm * cosBetak;
+//color += vec4(FinalTerm, 0.0); //tut nicht -> invalid operation
 }
-
